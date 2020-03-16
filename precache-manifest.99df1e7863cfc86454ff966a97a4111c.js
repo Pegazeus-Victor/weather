0f45babde2ef733d0a1db27b92d91209e4aaec14
Pegazeus-Victor <61429368+Pegazeus-Victor@users.noreply.github.com>
2020-03-16 12:57:37 -0700

    Updates

---
+++ precache-manifest.99df1e7863cfc86454ff966a97a4111c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9624923b87b0ed50067da91d8d2095",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "9e5ac649e61b3985ea81",
+    "url": "/weather/static/css/2.f75a4543.chunk.css"
+  },
+  {
+    "revision": "60d25344a80416906e9d",
+    "url": "/weather/static/css/main.2b479ba5.chunk.css"
+  },
+  {
+    "revision": "9e5ac649e61b3985ea81",
+    "url": "/weather/static/js/2.d298ef7c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.d298ef7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d25344a80416906e9d",
+    "url": "/weather/static/js/main.006f1a93.chunk.js"
+  },
+  {
+    "revision": "1e02fceca029e7e9b6fb",
+    "url": "/weather/static/js/runtime-main.6be21be7.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "38e99fdea918d575ee3ee00721eed095",
+    "url": "/weather/static/media/sonic 2.38e99fde.png"
+  },
+  {
+    "revision": "e641632c3044ff3cca4e1d8e46122f9f",
+    "url": "/weather/static/media/super sonic.e641632c.gif"
+  }
+]);
